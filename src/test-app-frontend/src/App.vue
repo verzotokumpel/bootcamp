ffa251d6daa54e7a425961a2a31eb24ccd5bf527
verzotokumpel <verzo.of@gmail.com>
2024-06-24 19:07:04 +0200

    added button xdd

--- src/test-app-frontend/src/App.vue
+++ src/test-app-frontend/src/App.vue
@@ -3,5 +3,23 @@
   <main>
     <img src="/logo2.svg" alt="DFINITY logo" />
     GMGMGMGMGMGM
+    <br/>
+    {{ counter }}
+    <button @click="increment">Increment</button>
   </main>
 </template>
+
+<script>
+  export default {
+    data(){
+      return {
+        counter: 0
+      }
+    },
+    methods: {
+      increment(){
+        this.counter++;
+      }
+    }
+  };
+</script>
